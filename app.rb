3b169ad82f280bef486c4bc1fdedb1686c31e5fb
Mackenzie Moore <macmoore@ucdavis.edu>
2019-04-16 18:18:23 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,11 +8,11 @@ class App < Sinatra::Base
   post '/reverse' do
     original_string = params["string"]
     @reversed_string = original_string.reverse
-    erb: reversed
+    erb :reversed
   end
 
   get '/friends' do
     # Write your code here!
   end
-  
+
 end
